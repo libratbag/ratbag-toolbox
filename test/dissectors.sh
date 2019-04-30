749942b62a867716e1374896d3a29c0165fd2fd5
FFY00 <filipe.lains@gmail.com>
2019-04-30 14:01:15 +0100

    tests: exit on LUA warnings

--- test/dissectors.sh
+++ test/dissectors.sh
@@ -2,9 +2,27 @@
 
 root="${BASH_SOURCE%/*}/.."
 
+declare -A hidpp_test_cases
+
 # HID++
-tshark -X lua_script:$root/dissectors/hidpp-dissector.lua -X lua_script1:1 -r $root/dumps/logitech/g500-init.pcapng # HID++ 1.0
-tshark -X lua_script:$root/dissectors/hidpp-dissector.lua -X lua_script1:1 -r $root/dumps/logitech/g502-init.pcapng # HID++ 1.0
-tshark -X lua_script:$root/dissectors/hidpp-dissector.lua -X lua_script1:1 -r $root/dumps/logitech/g603-init.pcapng # HID++ 1.0 (receiver) > HID++ 2.0 (device)
-tshark -X lua_script:$root/dissectors/hidpp-dissector.lua -X lua_script1:1 -r $root/dumps/logitech/unifying-5devices-kernel-init.pcapng # HID 1.0 (receiver)
+# dump file => HID++ version
+hidpp_test_cases=(
+  ['g500-init']=1				# HID++ 1.0
+  ['g502-init']=1				# HID++ 1.0
+  ['g603-init']=1				# HID++ 1.0 (receiver) > HID++ 2.0 (device)
+  ['unifying-5devices-kernel-init']=1		# HID++ 1.0 (receiver)
+)
+
+for dump in "${!hidpp_test_cases[@]}"; do
+  printf "Testing HID++ dissector against $dump.pcapng..."
+
+  output="$(tshark -X lua_script:"$root"/dissectors/hidpp-dissector.lua -X lua_script1:"${hidpp_test_cases[$dump]}" -r "$root"/dumps/logitech/"$dump".pcapng 2>&1 >/dev/null)"
+
+  if [[ -n "$output" ]]; then
+    printf ' FAILED\n\n'
+    echo "$output"
+  fi
+  printf '\n'
+
+done
 
